2dbd0759e92ebea4b73abbcef4a4b2e7872242d0
André Strhaug <andre.storhaug@mimer.no>
2017-01-18 23:20:33 +0100

    test

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /*global console*/
 function $(i) {
     "use strict";
@@ -19,17 +18,3 @@ function test() {
   var lol = "Dette er en test";
   console.log(lol);
 }
-=======
-/*global console*/
-function $(i) {
-    "use strict";
-    return document.querySelector(i);
-}
-document.addEventListener('DOMContentLoaded', function () {
-    "use strict";
-});
-function helloWorld() {
-    "use strict";
-    console.log("hello world");
-}
->>>>>>> master
