c53c04d9d7ab1f73bc73bcb8e0fa1abcccd55015
André Strhaug <andre.storhaug@mimer.no>
2017-01-19 00:01:45 +0100

    Cleanup

--- main.js
+++ main.js
@@ -6,12 +6,15 @@ function $(i) {
 document.addEventListener('DOMContentLoaded', function () {
     "use strict";
 });
+
+
+
+
+
 function helloWorld() {
     "use strict";
     console.log("hello world");
 }
-
-
 
 
 function test() {
