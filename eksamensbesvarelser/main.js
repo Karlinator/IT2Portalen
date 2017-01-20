87932475367489632ba55dd6dceef13917e607ff
Karl Erik Hofseth <Karl Erik Hofseth>
2017-01-20 10:59:57 +0100

    add debug

--- eksamensbesvarelser/main.js
+++ eksamensbesvarelser/main.js
@@ -1,6 +1,6 @@
 /*global console*/
 //List of the orders in the table.
-var orders = [];
+var orders = [], debug = true;
 function $(i) {
     "use strict";
     return document.querySelector(i);
@@ -13,6 +13,7 @@ function addRow() {
     children = $("#children").value;
     youth = $("#youth").value;
     adults = $("#adults").value;
+    if (debug) {console.log(week); }
     newOrder = {'week': week, 'dinners': dinners, 'children': children, 'youth': youth, 'adults': adults};
     orders.push(newOrder);
 }
