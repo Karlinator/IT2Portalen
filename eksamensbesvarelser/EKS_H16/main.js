19ecbddc173cf8ba90f29244b437158ef826ed0b
André Strhaug <andre.storhaug@mimer.no>
2017-01-20 12:40:45 +0100

    Oppg2 svae

--- eksamensbesvarelser/EKS_H16/main.js
+++ eksamensbesvarelser/EKS_H16/main.js
@@ -1,23 +1,56 @@
 window.onload = startup;
 
 var debug = true;
+var itemPrice;
 
 function startup() {
 
+ listenForChange();
 }
 
-// Run this function when something changes.
-function onchange() {
-    var selectedOpt = document.getElementById('numDinners').value;
+// Function listening for changes.
+function listenForChange() {
+    var dropdownNumDinners  = document.getElementById('numDinners');
 
+    var numPersons = document.getElementById('numPersons');
+
+    dropdownNumDinners.addEventListener("change", update);
+    numPersons.addEventListener("change", validate);
+
+
+
+if (debug) console.log("Enter function 'listenForChange'");
 }
 
 function validate() {
     var selectedOpt = document.getElementById('numDinners').value;
 
-    if (selectedOpt >= 10 && selectedOpt < 1) {
-        window.alert("Velg middag for et antall personer mellom 1 - 10");
+    if (selectedOpt >= 10 || selectedOpt < 1) {
+        alert("Velg middag for et antall personer mellom 1 - 10");
+        console.log("lol");
     }
 
-    if (debug) console.log("Enter function " + this);
+    if (debug) console.log("Enter function 'validate'." + " Validation passed");
+}
+
+function calculatePrice () {
+    var pricePrUnit;
+
+    var numPersons  = document.getElementById('numPersons').value;
+    var selectedOptNumDinners  = document.getElementById('numDinners').value;
+
+    if (numPersons <= 5) {
+      pricePrUnit = 70;
+    } else {
+      pricePrUnit = 80;
+    }
+}
+
+function update () {
+
+    var printedPrise  = document.getElementById('printedPrise');
+
+    printedPrise.innerHTML = itemPrice;
+
+  if (debug) console.log("Enter function 'update'");
 }
