b74057df7d87b52a991e7d6ab2fe2012ddca6f61
André Strhaug <andre.storhaug@mimer.no>
2017-01-20 13:06:26 +0100

    Oppgave 2 Nesten ferdig

--- eksamensbesvarelser/EKS_H16/main.js
+++ eksamensbesvarelser/EKS_H16/main.js
@@ -1,56 +1,69 @@
 window.onload = startup;
 
 var debug = true;
-var itemPrice;
 
 function startup() {
 
- listenForChange();
+    listenForChange();
 }
 
 // Function listening for changes.
 function listenForChange() {
-    var dropdownNumDinners  = document.getElementById('numDinners');
+    var dropdownNumDinners = document.getElementById('numDinners');
 
     var numPersons = document.getElementById('numPersons');
 
     dropdownNumDinners.addEventListener("change", update);
-    numPersons.addEventListener("change", validate);
+    numPersons.addEventListener("change", update);
 
 
 
-if (debug) console.log("Enter function 'listenForChange'");
+    if (debug) console.log("Enter function 'listenForChange'");
 }
 
 function validate() {
-    var selectedOpt = document.getElementById('numDinners').value;
-
-    if (selectedOpt >= 10 || selectedOpt < 1) {
-        alert("Velg middag for et antall personer mellom 1 - 10");
-        console.log("lol");
-    }
-
     if (debug) console.log("Enter function 'validate'." + " Validation passed");
+
+    var numPersons = document.getElementById('numPersons').value;
+
+    if (numPersons >= 10 || numPersons < 1) {
+        window.alert("Velg middag for et antall personer mellom 1 - 10");
+    } else {
+        return true;
+    }
 }
 
-function calculatePrice () {
-    var pricePrUnit;
+function calculatePrice() {
+    var itemPrice, pricePrUnit;
 
-    var numPersons  = document.getElementById('numPersons').value;
-    var selectedOptNumDinners  = document.getElementById('numDinners').value;
+    var numPersons = document.getElementById('numPersons').value;
+    var selectedOptNumDinners = document.getElementById('numDinners').value;
 
     if (numPersons <= 5) {
-      pricePrUnit = 70;
+        pricePrUnit = 70;
     } else {
-      pricePrUnit = 80;
+        pricePrUnit = 80;
     }
+
+    itemPrice = pricePrUnit * numPersons * selectedOptNumDinners;
+
+    return itemPrice;
 }
 
-function update () {
+function update() {
+    var price, validation;
 
-    var printedPrise  = document.getElementById('printedPrise');
+    price = calculatePrice();
+    console.log(price);
 
-    printedPrise.innerHTML = itemPrice;
+    validation = validate();
 
-  if (debug) console.log("Enter function 'update'");
+    if (validation) {
+
+        var printedPrise = document.getElementById('printedPrise');
+
+        printedPrise.innerHTML = price;
+    }
+
+    if (debug) console.log("Enter function 'update'");
 }
