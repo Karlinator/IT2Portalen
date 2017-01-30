ed07e89264bf51cfab3a8ab0e3905ed1e4009468
Karl Erik Hofseth <Karl Erik Hofseth>
2017-01-30 14:39:25 +0100

    Further code cleanup. Functions reorganised to improve readability.

--- eksamensbesvarelser/EKS_H16/receipt.js
+++ eksamensbesvarelser/EKS_H16/receipt.js
@@ -1,19 +1,12 @@
 /*global console*/
+//If set to true, the console outpust various messages in order to better find errors.
 var debug = true;
 
-function updateHTML(numPersons, numDinners, price, firstname, lastname, address) {
-    "use strict";
-    document.getElementById('numDinners').innerHTML = numDinners;
-    document.getElementById('numPersons').innerHTML = numPersons;
-    document.getElementById('price').innerHTML = price;
-    document.getElementById('firstname').innerHTML = firstname;
-    document.getElementById('lastname').innerHTML = lastname;
-    document.getElementById('address').innerHTML = address;
-
-    if (debug) {console.log("Enter function 'updateHTML'"); }
-}
+//Loads the order information and inserts it into the page.
 function loadEntries() {
     "use strict";
+    //Loads the order information entered in index.html.
+    //The order information was stored in session storage.
     var numDinners = sessionStorage.numDinners,
         numPersons = sessionStorage.numPersons,
         price = sessionStorage.price,
@@ -21,11 +14,22 @@ function loadEntries() {
         lastname = sessionStorage.lastname,
         address = sessionStorage.address;
 
-
-    updateHTML(numPersons, numDinners, price, firstname, lastname, address);
-
+    //Logs in the console. Useful for finding errors.
     if (debug) {console.log("Enter function 'loadEntries'"); }
+
+    //Inserts the order information into the document on screen.
+    //The order information is here retrieved from the variables to which they were bound above.
+    document.getElementById('numDinners').innerHTML = numDinners;
+    document.getElementById('numPersons').innerHTML = numPersons;
+    document.getElementById('price').innerHTML = price;
+    document.getElementById('firstname').innerHTML = firstname;
+    document.getElementById('lastname').innerHTML = lastname;
+    document.getElementById('address').innerHTML = address;
+
+    //Logs in the console. Useful for finding errors.
+    if (debug) {console.log("Enter function 'updateHTML'"); }
 }
+//Runs when the document is loaded. Initialises the script.
 document.addEventListener('DOMContentLoaded', function () {
     "use strict";
     loadEntries();
