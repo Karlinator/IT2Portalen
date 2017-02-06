13db836e1cdd064a53d5270571baf17f58637b79
Karl Erik Hofseth <Karl Erik Hofseth>
2017-02-06 14:43:11 +0100

    Fixed the names (see that André?).

--- js/footer.js
+++ js/footer.js
@@ -3,7 +3,7 @@ function generateFooter() {
 var footer;
 
     if(window.location.href.indexOf("eksamensbesvarelser") > -1) {
-      footer = '<div class="wrap"><span class="mega-octicon octicon-mark-github"></span><p><a href="../../index.html">IT2 Portalen</a> - André Storhaug, Johannes Berge, Karl Erik Hoffseth, Vegard &amp Torstein. All copyrights reserved.<br><a href="../../archive.html"><-- go back</a></p></div>';
+      footer = '<div class="wrap"><span class="mega-octicon octicon-mark-github"></span><p><a href="../../index.html">IT2 Portalen</a> - Andre Storhaug, Johannes Berge, Karl Erik Hofseth, Vegard Mauren Richardsen &amp Torstein Vik. All copyrights reserved.<br><a href="../../archive.html"><-- go back</a></p></div>';
 
     } else {
       footer = '<div class="wrap"><span class="mega-octicon octicon-mark-github"></span><p><a href="index.html">IT2 Portalen</a> - André Storhaug. All copyrights reserved.<br>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam a iaculis nibh.</p></div>';
